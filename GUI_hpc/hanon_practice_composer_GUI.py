4786c27e60fc714e27ee4c424eae8341643ca0dd
TetsuhiroKAWATA <philo.cosmo717@gmail.com>
2022-10-10 23:46:39 +0900

    GUI作成開始

--- GUI_hpc/hanon_practice_composer_GUI.py
+++ GUI_hpc/hanon_practice_composer_GUI.py
@@ -1,18 +1,50 @@
 import PySimpleGUI as sg
 
-#sg.theme('DarkAmber')
+
+#変数
+ans = ''#ユーザの答えを入れるやつ
+Opt = ['基礎練習(1~20)', 'スケール(39)', 'アルペジオ(41~43)', 'オクターブ(51,53,57)', '半音階(40)']
+
 sg.theme('BlueMono')
 
-layout = [ [sg.Text('Some text on row 1')],
-           [sg.Text('Enter something ong Row 2'), sg.InputText()],
-           [sg.Button('Ok'),sg.Button('Cancel')] ]
+top = [ [sg.Text('Hanon Practice Composer')],
+           [sg.Button('始める'), sg.Button('やめる')]]
 
-window = sg.Window('Hanon Practice Composer',layout)
+sele1 = [ [sg.Text('練習したい楽曲を選択してください')],
+          [sg.Text('書式:項目名(ハノン曲番号)')],
+          [sg.Text('現在選択中の項目:'), sg.Text(size = (20,1), key = '-OUTPUT-')],#sizeの第一引数で文字数、第二引数で行数を作ってるっぽい
+          [sg.Button(Opt[0], key = "0"), sg.Button(Opt[1], key = "1"),
+          sg.Button(Opt[2], key = "2"),sg.Button(Opt[3], key = "3"),
+          sg.Button(Opt[4], key = "4")],
+          [sg.Button('決定'), sg.Button('やめる')]]
+
+
+#処理
+window = sg.Window('Hanon Practice Composer',top)
 
 while True:
     event,values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
+    if event == sg.WIN_CLOSED or event == 'やめる':
         break
-    print('You entered ', values[0])
+    elif event == '始める':
+        window.close()
+        window = sg.Window('Hanon Practice Composer', sele1)
+        break
+
+while True:
+    event, values = window.read()
+    #print(event)
+    if event == sg.WIN_CLOSED or event == 'やめる':
+        break
+    elif event == '決定':#次のページへ
+        pass
+    else:#選択処理
+        ans = event
+        ans = int(ans)
+        #print(type(ans))#型確認
+        window['-OUTPUT-'].update(Opt[ans])
+
+
+
 
 window.close()
--- GUI_hpc/pytest.py
+++ GUI_hpc/pytest.py
@@ -1,12 +1,39 @@
 import PySimpleGUI as sg
 
-sg.theme('Dark Grey 13')
+sg.theme('Dark Blue 3')  # please make your windows colorful
 
-layout = [[sg.Text('Filename')],
-          [sg.Input(), sg.FileBrowse()],#FileBrowseってのを使うだけでPC内をブラウズできる
-          [sg.OK(), sg.Cancel()]]
+layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(12,1), key='-OUTPUT-')],
+          [sg.Input(key='-IN-')],
+          [sg.Button('Show'), sg.Button('Exit')]]
 
-window = sg.Window('Get filename example', layout)
+window = sg.Window('Window Title', layout)
 
-event,values = window.read()
+while True:  # Event Loop
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Show':
+        # change the "output" element to be the value of "input" element
+        window['-OUTPUT-'].update(values['-IN-'])
+
 window.close()
+
+
+#case2
+#text = sg.popup_get_text('inputer', 'Window title', 'Please input something')
+#sg.popup_animated('', 'wait', 'white', 'black')
+#sg.popup('Results','The value returned from PopupGetText', text)
+
+
+#case1
+#sg.theme('Dark Grey 13')
+
+#layout = [[sg.Text('Filename')],
+#          [sg.Input(), sg.FileBrowse()],#FileBrowseってのを使うだけでPC内をブラウズできる
+#          [sg.OK(), sg.Cancel()]]
+
+#window = sg.Window('Get filename example', layout)
+
+#event,values = window.read()
+#window.close()
